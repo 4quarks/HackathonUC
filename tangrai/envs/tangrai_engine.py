c67cbdb7a0ad7f140f9113ec2a02b4cf90d2ac5c
Pau <paucutrina@gmail.com>
2019-02-23 20:40:49 -0700

    Update

--- tangrai/envs/tangrai_engine.py
+++ tangrai/envs/tangrai_engine.py
@@ -1,165 +1,40 @@
-
-# Modified from Tetromino by lusob luis@sobrecueva.com
-# http://lusob.com
-# Released under a "Simplified BSD" license
-
+import numpy as np
 import random, time, pygame, sys
 from pygame.locals import *
 
+def getColors():
+    #               R    G    B
+#    white       = (255, 255, 255)
+#    gray        = (185, 185, 185)
+#    black       = (  0,   0,   0)
+    red         = (155,   0,   0)
+    lightred    = (175,  20,  20)
+    green       = (  0, 155,   0)
+    lightgreen  = ( 20, 175,  20)
+    blue        = (  0,   0, 155)
+    lightblue   = ( 20,  20, 175)
+    yellow      = (155, 155,   0)
+    lightyellow = (175, 175,  20)
+    
+    colors=(blue,green,red,yellow)
+    lightcolors=(lightblue,lightgreen,lightred,lightyellow)
+    
+    assert len(colors) == len(lightcolors)
+    
+    return colors,lightcolors
+
+colors,lightcolors= getColors()
+
+bgColor = (0,0,0) #Black
+
 FPS = 25
-BOXSIZE = 20
-BOARDWIDTH = 10
-BOARDHEIGHT = 20
-WINDOWWIDTH = BOXSIZE * BOARDWIDTH
-WINDOWHEIGHT = BOXSIZE * BOARDHEIGHT
-BLANK = '.'
+boxsize = 20
+boardWidth = 20
+boardHeigh = 20
+windoWidth = boxsize * boardWidth
+windowHeigh = boxsize * boardHeigh
 
-MOVESIDEWAYSFREQ = 0.15
-MOVEDOWNFREQ = 0.1
-
-XMARGIN = 0
-TOPMARGIN = 0
-
-#               R    G    B
-WHITE       = (255, 255, 255)
-GRAY        = (185, 185, 185)
-BLACK       = (  0,   0,   0)
-RED         = (155,   0,   0)
-LIGHTRED    = (175,  20,  20)
-GREEN       = (  0, 155,   0)
-LIGHTGREEN  = ( 20, 175,  20)
-BLUE        = (  0,   0, 155)
-LIGHTBLUE   = ( 20,  20, 175)
-YELLOW      = (155, 155,   0)
-LIGHTYELLOW = (175, 175,  20)
-
-BORDERCOLOR = BLUE
-BGCOLOR = BLACK
-TEXTCOLOR = WHITE
-TEXTSHADOWCOLOR = GRAY
-COLORS      = (BLUE,GREEN,RED,YELLOW)
-LIGHTCOLORS = (LIGHTBLUE, LIGHTGREEN, LIGHTRED, LIGHTYELLOW)
-assert len(COLORS) == len(LIGHTCOLORS) # each color must have light color
-
-TEMPLATEWIDTH = 5
-TEMPLATEHEIGHT = 5
-
-S_SHAPE_TEMPLATE = [['..OO.',
-                     '.OO..',
-                     '.....',
-                     '.....',
-                     '.....'],
-                    ['..O..',
-                     '..OO.',
-                     '...O.',
-                     '.....',
-                     '.....']]
-
-A_SHAPE_TEMPLATE = [['.OO..',
-                     '.OO..',
-                     '.OO..',
-                     '.....',
-                     '.....'],
-                    ['.....',
-                     '.OOO.',
-                     '.OOO.',
-                     '.....',
-                     '.....']]
-
-I_SHAPE_TEMPLATE = [['..O..',
-                     '..O..',
-                     '..O..',
-                     '..O..',
-                     '.....'],
-                    ['OOOO.',
-                     '.....',
-                     '.....',
-                     '.....',
-                     '.....']]
-
-O_SHAPE_TEMPLATE = [['.OO..',
-                     '.OO..',
-                     '.....',
-                     '.....',
-                     '.....']]
-
-J_SHAPE_TEMPLATE = [['.O...',
-                     '.OOO.',
-                     '.....',
-                     '.....',
-                     '.....'],
-                    ['..OO.',
-                     '..O..',
-                     '..O..',
-                     '.....',
-                     '.....'],
-                    ['.OOO.',
-                     '...O.',
-                     '.....',
-                     '.....',
-                     '.....'],
-                    ['..O..',
-                     '..O..',
-                     '.OO..',
-                     '.....',
-                     '.....']]
-
-L_SHAPE_TEMPLATE = [['...O.',
-                     '.OOO.',
-                     '.....',
-                     '.....',
-                     '.....'],
-                    ['..O..',
-                     '..O..',
-                     '..OO.',
-                     '.....',
-                     '.....'],
-                    ['.OOO.',
-                     '.O...',
-                     '.....',
-                     '.....',
-                     '.....'],
-                    ['.OO..',
-                     '..O..',
-                     '..O..',
-                     '.....',
-                     '.....']]
-
-T_SHAPE_TEMPLATE = [['..O..',
-                     '.OOO.',
-                     '.....',
-                     '.....',
-                     '.....'],
-                    ['..O..',
-                     '..OO.',
-                     '..O..',
-                     '.....',
-                     '.....'],
-                    ['.OOO.',
-                     '..O..',
-                     '.....',
-                     '.....',
-                     '.....'],
-                    ['..O..',
-                     '.OO..',
-                     '..O..',
-                     '.....',
-                     '.....']]
-
-PIECES = {'S': S_SHAPE_TEMPLATE,
-          'Z': A_SHAPE_TEMPLATE,
-          'J': J_SHAPE_TEMPLATE,
-          'L': L_SHAPE_TEMPLATE,
-          'I': I_SHAPE_TEMPLATE,
-          'O': O_SHAPE_TEMPLATE,
-          'T': T_SHAPE_TEMPLATE}
-
-shape = random.choice(list(PIECES.keys()))
-print(random.randint(0, len(PIECES[shape]) - 1))
-print(PIECES[0])
-
-
-A=['OOOOOO....',
+A=[['OOOOOO....',
    'OOOOOO....',
    'OOOOO.....',
    'OOOO......',
@@ -168,16 +43,211 @@ A=['OOOOOO....',
    'O.........',
    '..........',
    '..........',
-   '..........']
+   '..........']]
 
-import numpy as np
-Q=np.ones((10,10))
-x=5
-y=3
-for row in range(len(A[0])):
-    for column in range(len(A[0])):
-        if A[row][column]!='.':
-            try:
-                Q[row+x][column+y]=6
-            except:
-                print('NO!')
+
+B =[['..O.......',
+    '.OO.......',
+    'OOO.......',
+    'OOO.......',
+    'OO........',
+    'O.........',
+    '..........',
+    '..........',
+    '..........',
+    '..........']]
+
+C = [['....OO....',
+     '...OOOO...',
+     '..OOOOOO..',
+     '.OOOOOOOO.',
+     'OOOOOOOOOO',
+     '..........',
+     '..........',
+     '..........',
+     '..........',
+     '..........']]
+
+D = [['O.........',
+     'OO........',
+     'OO........',
+     'O.........',
+     '..........',
+     '..........',
+     '..........',
+     '..........',
+     '..........',
+     '..........']]
+
+E = [['.O........',
+     'OOO.......',
+     '.O........',
+     '..........',
+     '..........',
+     '..........',
+     '..........',
+     '..........',
+     '..........',
+     '..........']]
+
+F = [['OOOO......',
+     '.OO.......',
+     '..........',
+     '..........',
+     '..........',
+     '..........',
+     '..........',
+     '..........',
+     '..........',
+     '..........']]
+
+G = [['....O.....',
+     '...OO.....',
+     '..OOO.....',
+     '.OOOO.....',
+     'OOOOO.....',
+     '.OOOO.....',
+     '..OOO.....',
+     '...OO.....',
+     '....O.....',
+     '..........']]
+
+board=np.ones((10,10))
+
+boardWidth=10
+boardHeigh=10
+templeteWidth=10
+templeteHeigh=10
+
+blank='.'
+full='O'
+
+pieces = {'A': A,
+          'B': B,
+          'C': C,
+          'D': D,
+          'E': E,
+          'F': F,
+          'G': G}
+
+
+shape='A'
+rotation=0
+color=0
+
+x_Position=6
+y_Position=2
+
+class GameState:
+    global FPSclock, displayWindow, basicFont, bigFont
+    def __init__(self):
+#        pygame.init()
+#        FPSclock=pygame.time.Clock()  
+#        displayWindow = pygame.display.set_mode((windoWidth, windowHeigh))      
+#        basicFont = pygame.font.Font('freesansbold.ttf', 18)
+#        bigFont = pygame.font.Font('freesansbold.ttf', 100)
+#        pygame.display.iconify()
+#        pygame.display.set_caption('TangrAI') 
+        
+        self.board = self.getBlankBoard()
+        self.currentPiece = self.getNewPiece()
+        self.nextPiece = self.getNewPiece()
+
+#        pygame.display.update()
+        
+    def reinit(self):
+        self.board = self.getBlankBoard()
+        self.currentPiece = self.getNewPiece()
+        self.nextPiece = self.getNewPiece()
+
+#        pygame.display.update()
+        
+    def getActionSet(self):
+        return range(4)    
+    
+    def getReward(self):
+        self.counter_ones=0
+        for row in self.board:
+            for value in row:
+                if value==1:
+                    self.counter_ones+=1
+        return self.counter_ones
+    
+    def isGameOver(self):
+        return self.currentPiece == None and not self.isValidPosition()   
+    
+    def getNewPiece(self):
+        # return a random new piece in a random rotation and color
+        shape = random.choice(list(pieces.keys()))
+        newPiece = {'shape': shape,
+                    'rotation': random.randint(0, len(pieces[shape]) - 1),
+                    'x': int(boardWidth / 2) - int(templeteWidth / 2),
+                    'y': 0, # start it above the self.board (i.e. less than 0)
+                    'color': random.randint(0, len(colors)-1)}
+        return newPiece
+    
+    def addToBoard(self):
+        isOnBoard=True
+        global original_board
+        for row in range(templeteWidth):
+            for column in range(templeteHeigh):
+                if pieces[self.currentPiece['shape']][self.currentPiece['rotation']][row][column]!=blank:
+                    try:
+                        self.board[row+self.currentPiece['x']][column+self.currentPiece['y']]=self.currentPiece['color']
+                    except:
+                        isOnBoard=False
+                        
+        return isOnBoard
+     
+    def getBlankBoard(self):
+        self.board = []
+        for i in range(boardWidth):
+            self.board=np.append(self.board,[blank] * boardWidth)
+        self.board=np.reshape(self.board,(boardWidth,boardHeigh))
+        return self.board
+    
+    def isOnBoard(self,x,y):
+        return x >= 0 and x < boardWidth and y < boardHeigh
+
+    def piecetoMatrix(self):
+        self.piece_array=[]
+        for column in range(templeteHeigh):
+            print(pieces[self.currentPiece['shape']][self.currentPiece['rotation']][column])
+            for value in pieces[self.currentPiece['shape']][self.currentPiece['rotation']][column]:
+                if value ==blank:
+                    self.piece_array=np.append(self.piece_array,1)
+                else:
+                    self.piece_array=np.append(self.piece_array,self.currentPiece['color'])
+                    
+        self.piece_array=np.reshape(self.piece_array,(templeteWidth,templeteHeigh))
+        return self.piece_array
+    
+    def isValidPosition(self):
+        valid = True
+        peca_nova= piecetoMatrix()
+        for row in range(len(pieces[self.currentPiece['shape']][self.currentPiece['rotation']][0])):
+                for column in range(len(pieces[self.currentPiece['shape']][self.currentPiece['rotation']][0])):
+                    if peca_nova[row][column] == self.currentPiece['color']:
+                        if self.currentPiece['x']+row > 4 or self.currentPiece['y']+column > 4:
+                            valid = False
+                        else:
+                            if valid:
+                                if self.board[self.currentPiece['x']+row][self.currentPiece['y']+column]==1:
+                                    valid = True
+                                else:
+                                    valid = False    
+        return valid           
+    
+    def convertToStrBoard(self):
+        self.new_board=[]
+        for row in range(templeteWidth):
+            for column in range(templeteHeigh):
+                if board[row,column]==1:
+                    self.new_board=np.append(self.new_board,blank)
+                else:
+                    self.new_board=np.append(self.new_board,full)
+                    
+        self.new_board=np.reshape(self.new_board,(templeteWidth,templeteHeigh))
+        return self.new_board           
+
+
--- tangrai/envs/tangrai_env.py
+++ tangrai/envs/tangrai_env.py
@@ -1,17 +1,31 @@
-import gym
+import gym, numpy as np
 from gym import error, spaces, utils
 from gym.utils import seeding
+import tangrai_engine as game
 
+screenHeight=480
+screenWidth=480
 
 class tangraiENV(gym.Env):
-  metadata = {'render.modes': ['human']}
-
-  def __init__(self):
-    ...
-  def step(self, action):
-    ...
-    #return observation, reward , done, additional info
-  def reset(self):
-    ...
-  def render(self, mode='human', close=False):
-    ...
+    def __init__(self):
+        self.game_state = game.GameState()
+        self._action_set = self.game_state.getActionSet()
+        self.action_space = spaces.Discrete(len(self._action_set))
+        
+    def _step(self, a):
+        self._action_set = np.zeros([len(self._action_set)])
+        self._action_set[a] = 1
+        reward = 0.0
+        state, reward, terminal = self.game_state.frame_step(self._action_set)
+        return state, reward, terminal, {}
+    
+    @property
+    def _n_actions(self):
+        return len(self._action_set)
+    
+    def _reset(self):
+        do_nothing = np.zeros(len(self._action_set))
+        do_nothing[0] = 1
+        self.observation_space = spaces.Box(low=0, high=255, shape=(screenHeight, screenWidth, 3))
+        state, _, _= self.game_state.frame_step(do_nothing)
+        return state
